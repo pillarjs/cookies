1fdf2e13b3d789c2f05110eaa62b3eba7ba575db
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-10-03 23:37:32 -0400

    lint: add editorconfig and eslint to enforce

--- test/restify.js
+++ test/restify.js
@@ -1,9 +1,9 @@
-var assert = require('assert'),
-    restify = require('restify'),
-    keys = require('keygrip')(['a', 'b']),
-    http = require('http'),
-    Cookies = require('../'),
-    request = require('supertest')
+var assert = require('assert')
+  , restify = require('restify')
+  , keys = require('keygrip')(['a', 'b'])
+  , http = require('http')
+  , Cookies = require('../')
+  , request = require('supertest')
 
 describe('Restify', function () {
   var header
