9af9efffe554e1d70d8526fe1f9b50d611050e38
smcmurray <scmcmurray@gmail.com>
2018-05-19 19:51:34 -0700

    Allow maxAge of 0

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ Cookie.prototype.toString = function() {
 Cookie.prototype.toHeader = function() {
   var header = this.toString()
 
-  if (this.maxAge) this.expires = new Date(Date.now() + this.maxAge);
+  if (this.maxAge || 0 === maxAge) this.expires = new Date(Date.now() + this.maxAge);
 
   if (this.path     ) header += "; path=" + this.path
   if (this.expires  ) header += "; expires=" + this.expires.toUTCString()
